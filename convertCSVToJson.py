520d911b75a9ec51a3cb0cb71c621040b8dd1cd3
adityakanetkar26 <aditya.ykanetkar@gmail.com>
2015-10-10 18:41:43 -0400

    Convert to JSON

---
+++ convertCSVToJson.py
@@ -0,0 +1,27 @@
+import csv
+import simplejson
+import os
+
+
+def main():
+	csvFile = open("Data/SFBusinesses/businesses.csv", "r")
+	csvReader = csv.reader(csvFile)
+	csvData = list(csvReader)
+	fieldNames = tuple(csvData[0])
+	csvFile.close()
+
+	length = len(fieldNames)
+	jsonFile = open("Data/SFBusinesses/businesses.json", "w")
+	print len(csvData)
+
+	for x in csvData:
+		y = {}
+		i = 0
+		while i < length:
+			y[fieldNames[i]] = x[i]
+			i = i + 1
+		simplejson.dump(y, jsonFile)
+		jsonFile.write("\n")
+	jsonFile.close()
+
+main()
